1107fccbf67869941f94a33affdf49f90d09124a
jomabe <dmari.scfe11@gmail.com>
2017-05-17 19:53:59 +0200

    canvis de david

--- src/controlador/ControladorReservas.java
+++ src/controlador/ControladorReservas.java
@@ -1,5 +1,38 @@
 package controlador;
 
+import modelo.BD;
+import vista.NuevaReservaView;
+import vista.ReservasView;
+
 public class ControladorReservas {
 
+	private BD modelo;
+	private NuevaReservaView nr;
+	private ReservasView rv;
+	
+	
+	public ControladorReservas(BD modelo){
+		this.modelo = modelo;
+	}
+
+
+	public NuevaReservaView getNr() {
+		return nr;
+	}
+
+
+	public void setNr(NuevaReservaView nr) {
+		this.nr = nr;
+	}
+
+
+	public ReservasView getRv() {
+		return rv;
+	}
+
+
+	public void setRv(ReservasView rv) {
+		this.rv = rv;
+	}
+	
 }
--- src/modelo/dao/ReservaDAO.java
+++ src/modelo/dao/ReservaDAO.java
@@ -1,5 +1,37 @@
 package modelo.dao;
 
-public class ReservaDAO {
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import com.mysql.jdbc.Connection;
+
+import modelo.BD;
+import modelo.vo.ReservaVO;
+
+public class ReservaDAO {
+	BD bd;
+	ArrayList <ReservaVO> reservas;
+	
+	public void consultaReservas(BD modelo){
+		try {
+			Statement stmt = bd.getConexion().createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM reserva");
+			
+			while(rs.next()){
+				String codigo = rs.getString("codigo");
+				String cliente = rs.getString("cod_cliente");
+				String habitacion = rs.getString("cod_habitacion");
+				String inicio = rs.getString("inicio");
+				String fin = rs.getString("fin");
+				String noches = "LMAO";
+				String pension = rs.getString("regimen");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		ReservaVO r = new ReservaVO(codigo,cliente,habitacion,inicio,fin,pension)
+	}
 }
