50a670c23ab8003e55645b05d4c0d0d4c19b1ddf
1dam <1dam@gmail.com>
2017-05-18 20:22:27 +0200

    Clase idiomas

---
+++ src/idiomas/Idiomas.java
@@ -0,0 +1,37 @@
+package idiomas;
+
+import java.util.ResourceBundle;
+
+public class Idiomas {
+
+	static ResourceBundle bundle = null;
+	String var_idioma = new String("");
+	
+	
+	private Idiomas() {
+		//Esto no hace na
+		
+	}
+
+	public static ResourceBundle newIdioma(String idioma) {
+		
+		//Seleccion entre un idioma y otro
+		
+		if (bundle == null) {
+			if (idioma == "ingles") {
+				
+				bundle = ResourceBundle.getBundle("en_UK");
+			} else {
+			
+				bundle = ResourceBundle.getBundle("es_ES");
+			}
+		}
+		
+		return bundle;
+	}
+	
+	public ResourceBundle getBundle() {
+		
+		return bundle;
+	}
+}
