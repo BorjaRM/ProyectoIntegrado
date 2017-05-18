3101fb99530fb77f9c6b070f3c2b0bd0cc2f9769
srludy <jvt_92@hotmail.com>
2017-05-18 18:52:02 +0200

    Update Inicio.java

--- src/vista/Inicio.java
+++ src/vista/Inicio.java
@@ -16,14 +16,8 @@ public class Inicio {
 				try {
 					//Creamos la base de datos
 					BD database = BD.getSingleDBInstance("52.90.200.239","hotel_pi","usuario","Pintegrado2017");
-					Statement stmt = database.getConexion().createStatement();
-					//stmt.executeUpdate("INSERT INTO cliente VALUES (null, 'manolo', 'manolete', 'joder', '2017-01-01', '665848120', 'Española', 'jeje@pepe.com', '2022-10-03');");
-					//stmt.executeUpdate("DELETE FROM cliente WHERE codigo = '1';");
-					stmt.executeUpdate("UPDATE cliente SET nombre='pepe', apellidos='pepito', identificacion='toma', fecha_nacimiento='1992-02-03', nacionalidad='Marroqui', email='manoloNO', fecha_alta='2003-02-02' WHERE codigo='4';");
-
 					
-					
-					/*Creamos la primera ventana de la aplicacion
+					//Creamos la primera ventana de la aplicacion
 					LoginView login = new LoginView();
 					//Creamos el controlador principal
 					ControladorUsuarios cUsuarios = new ControladorUsuarios(database,login);
@@ -31,7 +25,7 @@ public class Inicio {
 					login.estableceControlador(cUsuarios);
 					//Visualizamos la ventana
 					login.setVisible(true);
-					*/		
+							
 				} catch (Exception e) {
 					//e.printStackTrace();
 					e.printStackTrace();
